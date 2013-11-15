fae56b52492acc54299277e0ee75873884c39eb1
Kristofer Borgström <k@kristoferb.se>
2013-11-15 19:32:36 +0100

    Introduced error detection

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 bankid-linux-installer
 ======================
 
-1. Download install-bankid.sh
-2. Right click the file, choose properties, switch to the permissions tab and check the executable box
-3. Doubleclick to run and choose execute in terminal
-4. Supply your password when asked
+Graphical Installation
+1. Download installation script: https://github.com/kribor/bankid-linux-installer/archive/master.zip
+2. Extract the zip file
+3. Right click the file install-bankid.sh, choose properties, switch to the permissions tab and check the "allow executing file as program" box
+4. Doubleclick to run and choose execute in terminal
+5. Supply your password when asked
+6. Clean up by removing the script and zip file
+
+Install in terminal, run:
+
+wget https://raw.github.com/kribor/bankid-linux-installer/master/install-bankid.sh
+chmod +x install-bankid.sh
+./install-bankid.sh
+rm -f install-bankid.sh
+
+
 
 Tested OSes
 ==============
-Ubuntu 12.04 Precise Pangolin
+Ubuntu 12.04 Precise Pangolin 32-bit
+Ubuntu 12.04 Precise Pangolin 64-bit
+Ubuntu 13.10 Saucy Salamander 32-bit
--- install-bankid.sh
+++ install-bankid.sh
@@ -1,16 +1,47 @@
 #!/bin/sh
 
+#Disabling checking of certificate since fresh ubuntu install doesnt seem to validate valid verisign certs 
+echo "Downloading latest official nexus package..."
+wget --no-check-certificate -O /tmp/nexus.tar.gz "https://install.bankid.com/Download?defaultFileId=Linux"
+if [ $? -ne 0 ]; then
+  echo "ERROR: Operation failed. Installation incomplete."
+  exit 1
+fi
 
-wget -O /tmp/nexus.tar.gz "https://install.bankid.com/Download?defaultFileId=Linux"
+echo "Download Complete"
+
+echo "Extracting nexus..."
 cd /tmp	
 tar xzf nexus.tar.gz
+if [ $? -ne 0 ]; then
+  echo "ERROR: Operation failed. Installation incomplete."
+  exit 1
+fi
+echo "Extraction completed"
+
+echo "Installing nexus"
 cd BISP-*
 sudo ./install* i
+if [ $? -ne 0 ]; then
+  echo "ERROR: Operation failed. Installation incomplete."
+  exit 1
+fi
 
-MACHINE_TYPE=`uname -m`
 if [ `getconf LONG_BIT` = "64" ]; then
+  echo "Detected 64-bit installation - installing additional packages"
   sudo apt-get install -y nspluginwrapper libstdc++6:i386 libidn11:i386
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Operation failed. Installation incomplete."
+    exit 1
+  fi
+  echo "Additional packages installed"
+  echo "Installing plugin wrapper..."
   sudo nspluginwrapper -i /usr/local/lib/personal/libplugins.so
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Operation failed. Installation incomplete."
+    exit 1
+  fi
+  echo "Plugin wrapper successfully installed"
 fi
 
 rm -rf /tmp/BISP*
